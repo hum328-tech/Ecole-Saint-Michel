6804653f0c842a7e0efc46e6bd6340b8ad3a5163
hum328-tech <hum328@gmail.com>
2026-06-23 18:35:58 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,1 @@
+const APP_CONFIG={schoolName:'École Saint-Michel',slogan:'GRANDIR SANS ARRÊT',logo:'assets/img/logo.png'};
---
+++ supabase.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL='https://ayfowlmeixqjoabsrtde.supabase.co';
+const SUPABASE_KEY='PEGA_AQUI_TU_SUPABASE_PUBLIC_ANON_KEY';
+const supabaseClient=supabase.createClient(SUPABASE_URL,SUPABASE_KEY);
